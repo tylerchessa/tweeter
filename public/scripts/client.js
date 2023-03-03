4d9353283ab12724f9d7a850a5eadb884f65597e
tylerchessa <tylerchessa@gmail.com>
2023-03-03 04:31:29 +0000

    more readable

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,18 +4,18 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
-
-const escape = function (str) {
+//function to prevent xss
+const escape = function(str) {
   let div = document.createElement("div");
   div.appendChild(document.createTextNode(str));
   return div.innerHTML;
 };
 
 $(document).ready(function() {
-const createTweetElement = (eachTweet) => {
-  const userT = eachTweet.user;
-  let $safeHTML = escape(eachTweet.content.text);
-  let markup = `
+  const createTweetElement = (eachTweet) => {
+    const userT = eachTweet.user;
+    let $safeHTML = escape(eachTweet.content.text);
+    let markup = `
   <article class="tweet">
   <header>
     <div class="tweet-profile"><img src=${userT.avatars}></img> ${userT.name}</div>
@@ -31,124 +31,110 @@ const createTweetElement = (eachTweet) => {
     </div>
   </footer>
 </article>
-`
-return markup
-};
+`;
+    return markup;
+  };
 
-
-const renderTweets = (tweets) => {
-  for (let tweet of tweets) {
-    createTweetElement(tweet)
-    const $tweet = createTweetElement(tweet);
-    $('#tweets').prepend($tweet);
-  }
-};
-
-
-// renderTweets(data);
-
-const loadTweets = () => {
-  $.ajax('/tweets', { method: 'GET' })
-  .then(function (tweets) {
-    $('#tweets').empty();
-    renderTweets(tweets);
-  });
-}
-loadTweets();
-
-  
-const characterError = () => {
-const tweet = $('#tweet-text').val()
-  if (!tweet) {
-    $('.error1').slideDown(1000)
-    return false;
-  }
-  if (tweet.length > 140) {
-    $('.error2').slideDown(1000)
-    return false;
-  }
-  $('.error1').slideUp(1000)
-  $('.error2').slideUp(1000)
-  return true;
-};
-
-
-$("#tweet-form").submit(function(event) {
-  event.preventDefault();
-  
-const tweet = $('#tweet-text').val()
-if (!tweet) {
-  $('.error1').slideDown(1000)
-  return false;
-}
-if (tweet.length > 140) {
-  $('.error2').slideDown(1000)
-  return false;
-}
-$('.error1').slideUp(1000)
-$('.error2').slideUp(1000)
-$.post( "/tweets", $(this).serialize() )
-.then(function () {
-  loadTweets();
-  $('#tweet-text').val('')
-  $('#tweet-text').parent().find('.counter').text(140);
-})
-});
-
-$("textarea").on("input", function () {
-const tweet = $('#tweet-text').val()
-if (tweet) {
-  $('.error1').slideUp(1000)
-}
-if (tweet.length <= 140) {
-  $('.error2').slideUp(1000)
-}
-});
-
-$('.navDiv').on('click', function() {
-  $('#tweet-form').slideToggle(1000);
-  $('#tweet-text').focus()
-});
-
-let $topBotton = $('#backToTop');
-$(window).on('scroll', function() {
-    if ($(window).scrollTop() > 500) {
-        $topBotton.fadeIn();
-    } else {
-        $topBotton.fadeOut();
+  //loops through all tweets and adds it to the HTML
+  const renderTweets = (tweets) => {
+    for (let tweet of tweets) {
+      createTweetElement(tweet);
+      const $tweet = createTweetElement(tweet);
+      $('#tweets').prepend($tweet);
     }
-});
-$topBotton.on('click', function(event) {
+  };
+
+
+  //makes a get request using AJAX (without refreshing the page)
+  const loadTweets = () => {
+    $.ajax('/tweets', { method: 'GET' })
+      .then(function(tweets) {
+        $('#tweets').empty();
+        renderTweets(tweets);
+      });
+  };
+  loadTweets();
+
+  //on submit will check to see if the user has atleast 1 character and less than 141 characters
+  //errors will show if criteria is not met and will not submit
+  $("#tweet-form").submit(function(event) {
+    event.preventDefault();
+    const tweet = $('#tweet-text').val();
+    if (!tweet) {
+      $('.error1').slideDown(1000);
+      return false;
+    }
+    if (tweet.length > 140) {
+      $('.error2').slideDown(1000);
+      return false;
+    }
+    $('.error1').slideUp(1000);
+    $('.error2').slideUp(1000);
+    $.post("/tweets", $(this).serialize())
+      .then(function() {
+        loadTweets();
+        $('#tweet-text').val('');
+        $('#tweet-text').parent().find('.counter').text(140);
+      });
+  });
+
+  //if user fixes error, the message will dissapear
+  $("textarea").on("input", function() {
+    const tweet = $('#tweet-text').val();
+    if (tweet) {
+      $('.error1').slideUp(1000);
+    }
+    if (tweet.length <= 140) {
+      $('.error2').slideUp(1000);
+    }
+  });
+
+  //upton clicking 'write a tweet' the text area will become in focus
+  $('.navDiv').on('click', function() {
+    $('#tweet-form').slideToggle(1000);
+    $('#tweet-text').focus();
+  });
+
+  //scroll to top button (stretch)
+  let $topBotton = $('#backToTop');
+  $(window).on('scroll', function() {
+    if ($(window).scrollTop() > 500) {
+      $topBotton.fadeIn();
+    } else {
+      $topBotton.fadeOut();
+    }
+  });
+  $topBotton.on('click', function(event) {
     event.preventDefault();
     $('html, body').animate({
-        scrollTop: 0
+      scrollTop: 0
     }, '500');
     $('#tweet-form').slideDown(1000);
-  $('#tweet-text').focus()
-});
+    $('#tweet-text').focus();
+  });
 
+  //will cause the nav background to be transparent for mobile devices ( < 1000 width ) 
+  //nav background will become opaque once the header background leaves the screen
+  let $backgroundNav = $('.nav');
+  $(window).on('scroll', function() {
+    if ($(window).width() < 1000) {
+      if ($(window).scrollTop() > 570) {
+        $backgroundNav.css("background-color", "#4056A1");
+      } else {
+        $backgroundNav.css("background-color", "initial");
+      }
+    } else {
+      $backgroundNav.css("background-color", '#4056A1');
+    }
+  });
 
-let $backgroundNav = $('.nav')
-$(window).on('scroll', function() {
-if ($(window).width() < 1000) {
-if ($(window).scrollTop() > 570) {
-  $backgroundNav.css("background-color", "#4056A1");
-} else {
-  $backgroundNav.css("background-color", "initial");
-}
-} else {
-  $backgroundNav.css("background-color", '#4056A1');
-}
-})
-
-$(window).resize(function() {
-  if ($(window).width() < 1000 && $(window).scrollTop() <= 570) {
-    $backgroundNav.css("background-color", "initial");
-  } else {
-    $backgroundNav.css("background-color", "#4056A1");
-  }
-});
-
-
-
+  //if nav background is transparent and window is resised from mobile to desktop. nav background becomes opaque. vice versa
+  $(window).resize(function() {
+    if ($(window).width() < 1000 && $(window).scrollTop() <= 570) {
+      $backgroundNav.css("background-color", "initial");
+    } else {
+      $backgroundNav.css("background-color", "#4056A1");
+    }
+  });
+  
 });
